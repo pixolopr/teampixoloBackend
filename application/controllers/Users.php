310f0add25998bdd95021215f6f831a41d565512
rahulpixolo <rahul.pixolo@gmail.com>
2015-08-12 16:43:56 +0530

    insert,update api

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -16,4 +16,22 @@
  	 	 $message['json']=$this->model->get_all(); 
  	 	 $this->load->view('json', $message); 
  	 } 
+ 	 public function addusers()
+ 	 {
+ 	 	$id = $this->input->get('id');
+ 	 	$name = $this->input->get('name');
+ 	 	$password = $this->input->get('password');
+ 	 	$access = $this->input->get('access');
+ 	 	$message['json'] = $this->model->addusers($id,$name,$password,$access);
+ 	 	$this->load->view('json', $message); 
+ 	 }
+ 	 public function updateusers()
+ 	 {
+ 	 	$id = $this->input->get('id');
+ 	 	$name = $this->input->get('name');
+ 	 	$password = $this->input->get('password');
+ 	 	$access = $this->input->get('access');
+ 	 	$message['json'] = $this->model->addusers($id,$name,$password,$access);
+ 	 	$this->load->view('json', $message); 
+ 	 }
  }
--- application/models/User_Model.php
+++ application/models/User_Model.php
@@ -6,6 +6,19 @@
 
  
  	 //Write functions here 
+ 	public function addusers($id,$name,$password,$access)
+ 	{
+ 		$query = $this->db->query("INSERT INTO `users` (`id`, `name`, `password`, `access`) VALUES ('$id', '$name', '$password', '$access')");
+ 		return $query;
+
+ 	}
+
+ 	public function updateusres($id,$name,$password,$access)
+ 	{
+ 		$query = $this->db->query("UPDATE `users` SET `id`= `$id`,`name`= `$name`,`password`= `$password`,`access`= `$access` WHERE `id`= $id");
+ 		return $query; 	
+ 		return trure;
+ 	}
  } 
  
  ?>
